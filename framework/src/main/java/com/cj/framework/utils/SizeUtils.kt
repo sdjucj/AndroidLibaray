06aa0acf0aaa51ae3c210e8250c820c164aefcf1
chenjing <864099641@qq.com>
2022-06-14 15:31:10 +0800

    tip:sizeUtils

---
+++ framework/src/main/java/com/cj/framework/utils/SizeUtils.kt
@@ -0,0 +1,61 @@
+package com.cj.framework.utils
+
+import android.content.res.Resources
+import android.util.DisplayMetrics
+
+/**
+ * desc:尺寸工具类
+ * Time:2022/4/22
+ * author:CJ
+ **/
+object SizeUtils {
+
+    /**
+     * Value of dp to value of px.
+     */
+    fun dp2px(dp: Float): Int {
+        val scale = Resources.getSystem().displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
+    /**
+     * Value of px to value of dp.
+     */
+    fun px2dp(px: Float): Int {
+        val scale = Resources.getSystem().displayMetrics.density
+        return (px / scale + 0.5f).toInt()
+    }
+
+    /**
+     * Value of sp to value of px.
+     */
+    fun sp2px(sp: Float): Int {
+        val fontScale = Resources.getSystem().displayMetrics.scaledDensity
+        return (sp * fontScale + 0.5f).toInt()
+    }
+
+    /**
+     * Value of sp to value of px.
+     */
+    fun px2sp(px: Float): Int {
+        val fontScale = Resources.getSystem().displayMetrics.scaledDensity
+        return (px / fontScale + 0.5f).toInt()
+    }
+
+    /**
+     * Value of screen width of px
+     */
+    fun getScreenWidth(): Int {
+        val dm: DisplayMetrics = Resources.getSystem().displayMetrics
+        return dm.widthPixels
+    }
+
+    /**
+     * Value of screen height of px
+     */
+    fun getScreenHeight(): Int {
+        val dm: DisplayMetrics = Resources.getSystem().displayMetrics
+        return dm.heightPixels
+    }
+
+}
